cff9c1b32506b25edfbcab678c26adaa5047aeba
pawv <pawelkie@gmail.com>
2021-10-14 10:56:30 +0200

    Add cancancan suport

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,8 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_post, only: %i[ show edit update destroy ]
+  load_and_authorize_resource
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
   end
 
   # GET /posts/1 or /posts/1.json
@@ -13,7 +11,6 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-    @post = Post.new
   end
 
   # GET /posts/1/edit
@@ -22,8 +19,6 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
-
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: "Post was successfully created." }
@@ -58,11 +53,6 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:title, :content)
